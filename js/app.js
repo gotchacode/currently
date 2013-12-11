0a2120d1f0ab6f529e0f99d232d611d623d0d564
Zach Olauson <zmolauson@gmail.com>
2013-12-11 13:45:27 -0600

    add weather toggle
    add ability to remove weather in layout options

--- js/app.js
+++ js/app.js
@@ -888,6 +888,10 @@ function style() {
       $('#main').addClass('no-seconds');
     }
 
+    // Remove weather
+    if (!options.weather) {
+      $('#main #weather').addClass('hidden');
+    }
   });
 }
 
