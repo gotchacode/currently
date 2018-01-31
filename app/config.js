d9fdc2e6ac90fe0b14b3d18b8871ded6e51ddd37
Vinit Kumar <vinit1414.08@bitmesra.ac.in>
2018-02-01 00:00:13 +0530

    update latest version

---
+++ app/config.js
@@ -0,0 +1,4 @@
+angular.module('currently.config', [])
+.constant('WEATHER_UNDERGROUND_API_KEY', "846edca2fe64735c")
+.constant('WELCOME_SCREEN', "3.0.0")
+.constant('RAVEN_CONFIG', {"dsn":"https://9701bebdd27d47e49b6bdcd60e07a0ba@app.getsentry.com/40011","config":{"fetchContext":false}});
